8a03981d8f861900b1ddfdb40f77130d0cc73097
Anthony felipe <anthonyfelipe149@gmail.com>
2026-06-24 12:13:21 -0300

    exercicio sobre: ✅ Arrays                  ✅ Estruturas de repetição (for)                  ✅ Condicionais (if/else)                  ✅ Manipulação de variáveis                  ✅ Resolução de problemas

---
+++ src/com/estudosjava/logica/Exercicio14.java
@@ -0,0 +1,14 @@
+package com.estudosjava.logica;
+
+public class Exercicio14 {
+    public static void main(String[] args) {
+        double[] precos = {20.50, 80.00, 15.00, 120.30, 45.00, 60.00};
+        double somaTotal = 0;
+        for( int i = 0; i < precos.length; i++){
+            if( precos[i] > 50.00){
+                somaTotal += precos[i];
+            }
+        }
+        System.out.println(somaTotal);
+    }
+}
